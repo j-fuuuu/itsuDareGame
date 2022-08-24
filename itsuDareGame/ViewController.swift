7b05173bdf0fd1f699bb75e18dcce5d999b23957
kumi fujii <kuum9393@gmail.com>
2022-08-24 15:25:49 +0900

    Initial Commit

---
+++ itsuDareGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itsuDareGame
+//
+//  Created by 藤井玖光 on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
